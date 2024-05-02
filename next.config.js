2ea084160e3c78e251c7aa4fbb6a3e154b21cc7f
UjjwalNatani <ujjwalnatani10@gmail.com>
2024-05-02 20:30:10 +0530

    updated

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    env: {
-        MONGO_URL: "mongodb + srv://ujjwalnatani10:Ugnatani@cluster0.ijnxryd.mongodb.net/",
-        DOMAIN: "http://localhost:8000/",
-        PORT: "8000",
-    },
-}
