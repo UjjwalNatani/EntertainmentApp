4d699064a6c9ca84607f0636370ba42cfc664896
UjjwalNatani <ujjwalnatani10@gmail.com>
2024-05-03 11:57:01 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     env: {
         // Define your environment variables here
         MONGO_URL: "mongodb+srv://ujjwalnatani10:Ugnatani@cluster0.ijnxryd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-        DOMAIN: "http://localhost:8000/",
-        PORT: 8000
+        DOMAIN: "https://entertainment-app-ruddy.vercel.app/",
+        PORT: process.env.PORT
     },
 };
